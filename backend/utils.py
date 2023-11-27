8778017b5ba0ab20e3ebb5ff6358b361cc2b85dc
outpost-caprice <itani@outpost-corp.com>
2023-11-27 12:20:43 +0900

    modified:   backend/utils.py     fix make PDF

--- backend/utils.py
+++ backend/utils.py
@@ -1,22 +1,30 @@
+import os
 import aiofiles
 import urllib
 import uuid
 from md2pdf.core import md2pdf
 
 async def write_to_file(filename: str, text: str) -> None:
-    """Asynchronously write text to a file in UTF-8 encoding.
-
-    Args:
-        filename (str): The filename to write to.
-        text (str): The text to write.
-    """
-    # Convert text to UTF-8, replacing any problematic characters
     text_utf8 = text.encode('utf-8', errors='replace').decode('utf-8')
-
     async with aiofiles.open(filename, "w", encoding='utf-8') as file:
         await file.write(text_utf8)
 
+async def create_css_file(css_file_path: str) -> None:
+    css_text = """
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap');
+
+    body {
+      font-family: 'Noto Sans', sans-serif;
+    }
+    """
+    if not os.path.exists(css_file_path):
+        async with aiofiles.open(css_file_path, "w") as file:
+            await file.write(css_text)
+
 async def write_md_to_pdf(text: str) -> str:
+    css_file_path = "your_css_file.css"
+    await create_css_file(css_file_path)
+
     task = uuid.uuid4().hex
     file_path = f"outputs/{task}"
     await write_to_file(f"{file_path}.md", text)
@@ -25,7 +33,7 @@ async def write_md_to_pdf(text: str) -> str:
         md2pdf(f"{file_path}.pdf",
                md_content=None,
                md_file_path=f"{file_path}.md",
-               css_file_path="your_css_file.css",  # ここにCSSファイルのパスを指定
+               css_file_path=css_file_path,
                base_url=None)
         print(f"Report written to {file_path}.pdf")
     except Exception as e:
